65ae5c40f77663f1c8ec10d48a74b07cafc7658a
Maria <maroulaaki@gmail.com>
2023-08-11 15:21:42 +0300

    add LeMoC code and files

---
+++ LeMoC/LeMoC.py
@@ -0,0 +1,285 @@
+# This is the leptonic version of a radiative transfer code LeHaMoC developed by S. I. Stathopoulos 
+# (in collaboration with M. Petropoulou). If using this code, make reference to the
+# following publication: Stathopoulos et al., 2023, A&A
+ 
+
+import numpy as np
+import astropy.units as u
+from astropy import constants as const 
+from astropy.modeling.models import BlackBody
+import pandas as pd
+import sys
+from tqdm import tqdm
+
+
+#######################
+#constants# 
+#######################
+G = (const.G).cgs.value       
+c = (const.c).cgs.value     
+Ro = (const.R_sun).cgs.value            
+Mo = (const.M_sun).cgs.value       
+yr = (u.yr).to(u.s)                
+kpc = (u.kpc).to(u.cm)             
+pc = (u.pc).to(u.cm)              
+m_pr = (u.M_p).to(u.g)         
+m_el = (u.M_e).to(u.g)         
+kb = (const.k_B).cgs.value
+h = (const.h).cgs.value 
+q = (const.e.gauss).value                
+sigmaT = (const.sigma_T).cgs.value               
+eV = (u.eV).to(u.erg)   
+B_cr = 2*np.pi*m_el**2*c**3/(h*q)
+################################
+
+if len(sys.argv) != 3:
+    print('incorrect parameters passed')
+    print('try something like this')
+    print('python LeMoC.py Parameters.txt out1')
+    quit()
+
+#Read parameters file
+fileName = sys.argv[1] 
+out_s = sys.argv[2]
+
+#Define output files
+out1 = "Particles_Distribution_"+out_s+".txt"
+out2 = "Photons_Distribution_"+out_s+".txt"
+
+
+fileObj = open(fileName)
+params = {}
+for line in fileObj:
+    line=line.strip()
+    key_value = line.split("=")
+    params[key_value[0].strip()] = float(key_value[1].strip())
+    
+time_init = float(params['time_init']) #R0/c
+time_end = float(params['time_end']) #R0/c
+step_alg = float(params['step_alg']) #R0/c
+g_min_el = float(params['g_min_el']) #log
+g_max_el = float(params['g_max_el']) #log
+g_PL_min = float(params['g_PL_min']) #log
+g_PL_max = float(params['g_PL_max']) #log
+grid_g_el = float(params['grid_g_el'])
+grid_nu = float(params['grid_nu'])
+p_el = float(params['p_el'])
+L_el = float(params['L_el']) #log
+Vexp = float(params['Vexp'])*c #/c
+R0 = 10**float(params['R0']) #log
+B0 = float(params['B0'])
+m = float(params['m'])
+delta = float(params["delta"])
+inj_flag = float(params['inj_flag'])
+Ad_l_flag = float(params['Ad_l_flag'])
+Syn_l_flag = float(params['Syn_l_flag'])
+Syn_emis_flag = float(params['Syn_emis_flag'])
+IC_l_flag = float(params['IC_l_flag'])
+IC_emis_flag = float(params['IC_emis_flag'])
+SSA_l_flag = float(params['SSA_l_flag'])
+gg_flag = float(params['gg_flag'])
+esc_flag = float(params['esc_flag'])
+BB_flag = float(params['BB_flag'])
+temperature = 10**float(params['BB_temperature']) #log 
+GB_ext = float(params['GB_ext'])
+PL_flag = float(params['PL_flag'])
+dE_dV_ph = float(params['dE_dV_ph'])
+nu_min_ph = float(params['nu_min_ph'])
+nu_max_ph = float(params['nu_max_ph'])
+s_ph = float(params['s_ph'])
+User_ph = float(params['User_ph'])
+
+import LeMoC_f as f # imports functions
+ 
+import time
+start_time = time.time()
+
+time_real = time_init
+dt = step_alg*R0/c # time step used for solving the PDE
+day_counter = 0.
+comp_el = sigmaT*10**L_el/(4.*np.pi*R0*m_el*c**3) # intial electron compactness
+Radius = R0
+
+# initialization of the electron Lorentz factor array
+grid_size = grid_g_el
+g_el = np.logspace(g_min_el,g_max_el,int(grid_size))
+g_el_mp = np.array([(g_el[im+1]+g_el[im-1])/2. for im in range(0,len(g_el)-1)])
+dg_el = np.array([((g_el[im+1])-(g_el[im-1]))/2. for im in range(1,len(g_el)-1)])   # delta gamma 
+dg_l_el = np.log(g_el[1])-np.log(g_el[0]) # logarithmic delta gamma
+
+if g_PL_max == g_max_el:
+    index_PL_max = -1
+else:
+    index_PL_max = min(min(np.where(g_el > 10**g_PL_max)))
+    
+if g_PL_min == 0.:
+    index_PL_min = 1
+else: 
+    index_PL_min = max(max(np.where(g_el < 10**g_PL_min)))
+
+# initialization of photon frequency arrays    
+nu_syn = np.logspace(7.5,np.log10(7.*f.nu_c(g_el[-1],B0))+1.4,int(grid_size/2))
+nu_ic = np.logspace(10.,30.,int(grid_size/2))
+nu_tot = np.logspace(np.log10(nu_syn[0]),np.log10(nu_ic[-1]),int(grid_nu))
+a_gg_f = np.zeros(len(nu_ic))
+
+#External grey body (GB) photon field (if GB_ext = 1 then photon spectrum is BB with the given temperature)
+#Units (nu,dN/dVdnu)
+if BB_flag == 0.:
+    dN_dVdnu_BB = np.zeros(2)
+    nu_bb = np.array([nu_syn[0], nu_syn[-1]])
+else:
+    bb = BlackBody(temperature*u.K)
+    nu_bb = np.array(np.logspace(np.log10(5.879*10**10*temperature)-6., np.log10(5.879*10**10*temperature)+1.5,60)*u.Hz)
+    photons_bb = np.array(4.*np.pi/c*bb(nu_bb)/(h*nu_bb))                       
+    GB_norm = np.trapz(photons_bb*h*nu_bb**2.,np.log(nu_bb))/(GB_ext) 
+    dN_dVdnu_BB = photons_bb/GB_norm
+                     
+#External power law (PL) photon field      
+#Units (nu,dN/dVdnu)        
+if PL_flag == 0.:
+    dN_dVdnu_pl = np.zeros(len(nu_tot))
+else:
+    nu_ph_ext_sp = np.logspace(nu_min_ph,nu_max_ph,100)
+    k_ph = (np.trapz(dE_dV_ph*nu_ph_ext_sp**(-s_ph+1.)))**(-1.)
+    nu_ph_ext_sp[-1] = 0.
+    dN_dVdnu_pl = 10**np.interp(np.log10(nu_tot),np.log10(nu_ph_ext_sp),np.log10(k_ph*nu_ph_ext_sp**(-s_ph)))
+
+#External user-defined photon field              
+if User_ph == 0.:
+    dN_dVdnu_user = np.zeros(len(nu_tot))
+else:  
+    #Units (nu,dN/dVdnu)
+    Photons_spec_user = pd.read_csv('Photons_spec_user.txt',names=('logx','logy'),sep=",")
+    nu_user = 10**np.array(Photons_spec_user.logx)
+    dN_dVdnu_user_temp = 10**np.array(Photons_spec_user.logy)
+    dN_dVdnu_user_temp[-1] = 10**(-160.)
+    dN_dVdnu_user = 10**np.interp(np.log10(nu_tot),np.log10(nu_user),np.log10(dN_dVdnu_user_temp))
+                    
+#Initialize arrays for particles and photons
+N_el = np.zeros(len(g_el)) # Number of electrons & positrons
+Q_ee = np.zeros(len(g_el)-1) # Pair production rate
+el_inj = np.ones(len(g_el))*10**(-260.) # Primary electron injection rate 
+el_inj[index_PL_min:index_PL_max] = f.Q_el_Lum(f.Lum_e_inj(comp_el,Radius),p_el,g_el[index_PL_min],g_el[index_PL_max])*g_el[index_PL_min:index_PL_max]**(-p_el)
+N_el = el_inj.copy()
+N_el[0] = N_el[-1] = 10**(-260.) # boundary conditions 
+
+photons_syn = np.ones(len(nu_syn))*10**(-260.)
+photons_IC = np.ones(len(nu_ic))*10**(-260.)
+
+photons_syn = np.append(photons_syn,10**(-260.))
+dN_dVdnu_BB = np.append(dN_dVdnu_BB,10**(-260.))
+
+nu_syn = np.append(nu_syn,nu_tot[-1])
+nu_bb = np.append(nu_bb,nu_tot[-1])
+
+nu_syn_mp = np.array([(nu_syn[im+1]+nu_syn[im-1])/2. for im in range(0,len(nu_syn)-1)])
+nu_ic_mp = np.array([(nu_ic[im+1]+nu_ic[im-1])/2. for im in range(0,len(nu_ic)-1)])
+dnu = np.array([(nu_syn[nu_ind+1]-nu_syn[nu_ind-1])/2. for nu_ind in range(1,len(nu_syn)-1)])
+dnu_ic = np.array([(nu_ic[nu_ind+1]-nu_ic[nu_ind-1])/2. for nu_ind in range(1,len(nu_ic)-1)])
+
+ 
+# Solution of the PDEs
+with open(out1,'w') as f1, open(out2,'w') as f2:
+    for i in tqdm(range(int(time_end)),desc="Progress..."):
+    # while time_real <  time_end*R0/c: 
+        time_real += dt    
+        Radius = f.R(R0,time_real,time_init,Vexp)
+        M_F = f.B(B0,R0,Radius,m)
+        a_cr_el = 3.*q*M_F/(4.*np.pi*m_el*c)
+        
+        # Calculate total dN/dVdν
+        photons = f.photons_tot(nu_syn,nu_bb,photons_syn,nu_ic,photons_IC,nu_tot,dN_dVdnu_BB*f.Volume(Radius),dN_dVdnu_pl*f.Volume(Radius),dN_dVdnu_user*f.Volume(Radius))/f.Volume(Radius)
+        
+        if Ad_l_flag == 1.:
+            b_ad = Vexp/Radius
+            dgdt_ad_el_m = b_ad*np.divide(np.power(g_el_mp[0:-1],1.),dg_el)
+            dgdt_ad_el_p = b_ad*np.divide(np.power(g_el_mp[1:],1.),dg_el)
+            dnudt_ad_syn_m = b_ad*np.divide(nu_syn_mp[0:-1],dnu)
+            dnudt_ad_syn_p = b_ad*np.divide(nu_syn_mp[1:],dnu)
+            dnudt_ad_IC_m = b_ad*np.divide(nu_ic_mp[0:-1]-nu_ic[:-2],dnu_ic)
+            dnudt_ad_IC_p = b_ad*np.divide(nu_ic[:1],dnu_ic)
+        else:
+            dgdt_ad_el_m = np.zeros(len(g_el)-2)
+            dgdt_ad_el_p = np.zeros(len(g_el)-2)
+            dnudt_ad_syn_m = np.zeros(len(nu_syn)-2)
+            dnudt_ad_syn_p = np.zeros(len(nu_syn)-2)
+            dnudt_ad_IC_m = np.zeros(len(nu_ic)-2)
+            dnudt_ad_IC_p = np.zeros(len(nu_ic)-2)
+        
+        if Syn_l_flag == 1.:
+            b_syn_el = (4./3.)*sigmaT/(8.*np.pi*m_el*c)*M_F**2.
+            dgdt_Syn_el_m = b_syn_el*np.divide(np.power(g_el_mp[0:-1],2.),dg_el)
+            dgdt_Syn_el_p = b_syn_el*np.divide(np.power(g_el_mp[1:],2.),dg_el)
+        else :
+            dgdt_Syn_el_m = np.zeros(len(g_el)-2)      
+            dgdt_Syn_el_p = np.zeros(len(g_el)-2)  
+    
+        if IC_l_flag == 1.:
+            U_ph = f.U_ph_f(g_el,nu_tot,photons,Radius)
+            b_Com_el = 4./3.*sigmaT*np.multiply(c,U_ph)/(m_el*c**2.)
+            dgdt_IC_el_m = b_Com_el[1:-1]*np.divide(np.power(g_el_mp[0:-1],2.),dg_el)
+            dgdt_IC_el_p = b_Com_el[2:]*np.divide(np.power(g_el_mp[1:],2.),dg_el)
+        else:
+            dgdt_IC_el_m = np.zeros(len(g_el)-2)    
+            dgdt_IC_el_p = np.zeros(len(g_el)-2) 
+    
+        V1 = np.zeros(len(g_el)-2)
+        V2 = 1.+dt*(c/Radius*esc_flag+dgdt_Syn_el_m+dgdt_IC_el_m+dgdt_ad_el_m)
+        V3 = -dt*(dgdt_Syn_el_p+dgdt_IC_el_p+dgdt_ad_el_p) 
+        if inj_flag == 1.:
+            S_ij = N_el[1:-1]+np.multiply(el_inj[1:-1],dt)+np.multiply(Q_ee[1:],dt)*f.Volume(Radius)
+        if inj_flag == 0.:
+            S_ij = N_el[1:-1]+np.multiply(Q_ee[1:],dt)*f.Volume(Radius)
+            
+        N_el[1:-1] = f.thomas(V1, V2, V3, S_ij)    
+        dN_el_dVdg_el = np.array(N_el/f.Volume(Radius))
+    
+        if Syn_emis_flag == 1.:
+            Q_Syn_el = np.divide([f.Q_syn_space(dN_el_dVdg_el,M_F,nu_syn[nu_ind],a_cr_el,g_el) for nu_ind in range(len(nu_syn)-1)], f.cor_factor_syn_el(g_el,R0,10**4.,p_el,f.Lum_e_inj(comp_el,R0)))
+        else: 
+            Q_Syn_el = np.zeros(len(nu_syn)-1)
+    
+        if IC_emis_flag == 1.:
+            Q_IC = [f.Q_IC_space_optimized(dN_el_dVdg_el,g_el,nu_ic[nu_ind],photons,nu_tot,len(nu_tot)-1) for nu_ind in range(0,len(nu_ic)-1)]
+        else:
+            Q_IC = np.zeros(len(nu_ic)-1)  
+    
+        if SSA_l_flag == 1.:
+            aSSA_space_syn = [-np.absolute(f.aSSA(dN_el_dVdg_el,M_F,nu_syn[nu_ind],g_el,dg_l_el)) for nu_ind in range(0,len(nu_syn-1))]
+            aSSA_space_ic = [-np.absolute(f.aSSA(dN_el_dVdg_el,M_F,nu_ic[nu_ind],g_el,dg_l_el)) for nu_ind in range(0,len(nu_ic-1))]
+        else:
+            aSSA_space_syn = np.zeros(len(nu_syn-1)) 
+            aSSA_space_ic = np.zeros(len(nu_ic-1)) 
+    
+        V1 = np.zeros(len(nu_syn)-2)
+        V2 = 1.+dt*(c/Radius+dnudt_ad_syn_m-np.multiply(aSSA_space_syn[1:-1],1)*c)
+        V3 = -dt*dnudt_ad_syn_p
+        S_ij = photons_syn[1:-1]+4.*np.pi*np.multiply(Q_Syn_el,dt)[1:]*f.Volume(Radius)
+        photons_syn[1:-1] = f.thomas(V1, V2, V3, S_ij)  
+    
+        V1 = np.zeros(len(nu_ic)-2)
+        V2 = 1.+dt*(c/Radius+dnudt_ad_IC_m+np.multiply(a_gg_f[1:-1],c)-np.multiply(aSSA_space_ic[1:-1],1)*c)
+        V3 = -dt*dnudt_ad_IC_p
+        S_ij = photons_IC[1:-1]+np.multiply(Q_IC,dt)[1:]*f.Volume(Radius)
+        photons_IC[1:-1] = f.thomas(V1, V2, V3, S_ij )
+        
+        if gg_flag == 0.:
+            a_gg_f = np.zeros(len(nu_ic))
+        else: 
+            a_gg_f = f.a_gg(nu_ic,nu_tot,photons)
+            Q_ee = f.Q_ee_f(nu_tot,photons,nu_ic,photons_IC/f.Volume(Radius),g_el,Radius)                  
+
+        if day_counter<time_real:
+            day_counter=day_counter+step_alg*R0/c
+            photons = f.photons_tot(nu_syn,nu_bb,photons_syn,nu_ic,photons_IC,nu_tot,dN_dVdnu_BB*f.Volume(Radius),dN_dVdnu_pl*f.Volume(Radius),dN_dVdnu_user*f.Volume(Radius))/f.Volume(Radius)
+            Spec_temp_tot = np.multiply(photons,h*nu_tot**2.)*4.*np.pi/3.*Radius**2.*c  
+            pr1 = [[str(el_list) for el_list in np.log10(g_el) ],[str(el_list) for el_list in np.log10(N_el/f.Volume(Radius)) ]]
+            pr2 = [[str(el_list) for el_list in np.log10(nu_tot*delta) ],[str(el_list) for el_list in np.log10(Spec_temp_tot) ]]
+            # here is where you unpack everything
+            for row in zip(*pr1):
+                f1.write(' '.join(row) + '\n')
+            for row in zip(*pr2):
+                f2.write(' '.join(row) + '\n')
+        
+print("--- %s seconds ---" % "{:.2f}".format((time.time() - start_time)))
---
+++ LeMoC/LeMoC_f.py
@@ -0,0 +1,230 @@
+import numpy as np
+from shapely.geometry import  LineString
+from astropy import units as u
+from astropy import constants as const 
+
+#######################
+#constants# 
+#######################
+G = (const.G).cgs.value       
+c = (const.c).cgs.value     
+Ro = (const.R_sun).cgs.value            
+Mo = (const.M_sun).cgs.value       
+yr = (u.yr).to(u.s)                
+kpc = (u.kpc).to(u.cm)             
+pc = (u.pc).to(u.cm)              
+m_pr = (u.M_p).to(u.g)         
+m_el = (u.M_e).to(u.g)         
+kb = (const.k_B).cgs.value
+h = (const.h).cgs.value 
+q = (const.e.gauss).value                
+sigmaT = (const.sigma_T).cgs.value               
+eV = (u.eV).to(u.erg)   
+B_cr = 2*np.pi*m_el**2*c**3/(h*q)
+
+
+#######################
+#functions# 
+#######################
+
+# expanding source radius 
+def R(R0,t,t_i,Vexp):
+    return R0+Vexp*(t-t_i)
+
+# magnetic field strength
+def B(B0,R0,R,m):
+    return B0*(R0/R)**m
+
+# volume of spherical source
+def Volume(R):
+    return 4.*np.pi/3.*R**3. 
+
+
+#Electron normalization for a power-law injection with sharp off
+def Q_el_Lum(L_el,p_el,g_min_el,g_max_el):
+    if p_el == 2.:
+        return L_el/(m_el*c**2.*np.log(g_max_el/g_min_el)) 
+    else:
+        return L_el*(-p_el+2.)/(m_el*c**2.*(g_max_el**(-p_el+2.)-g_min_el**(-p_el+2.)))
+
+# Converts compactness to luminosity
+def Lum_e_inj(comp,R):
+    return 4.*np.pi*R*m_el*c**3.*comp/(sigmaT)
+
+# Converts luminosity to flux
+def nuF_nu_obs(nu_L_nu,Dist_in_pc,delta):
+    return np.multiply(nu_L_nu,delta**4.)/(4.*np.pi*(Dist_in_pc*pc)**2.)
+
+#Synchrotron critical frequency (Radiative Processes in Astrophysics, by George B. Rybicki, Alan P. Lightman, Wiley-VCH , June 1986.)
+def nu_c(gamma,B):
+    return 3./(4.*np.pi)*q*B/(m_el*c)*gamma**2.
+
+#Synchrotron emissivity dN/dVdνdtdΩ (Relativistic Jets from Active Galactic Nuclei, by M. Boettcher, D.E. Harris, ahd H. Krawczynski, Berlin: Wiley, 2012)
+def Q_syn_space(Np,B,nu,a_cr,g):                      
+    C_syn = sigmaT*c/(h*24.*np.pi**2.*0.8975)*(4.*np.pi*m_el*c/(3.*q))**(4./3.)
+    syn_em = C_syn*B**(2./3.)*nu**(-2./3.)*(np.trapz(Np*g**(1./3.)*np.exp(-nu/(a_cr*g**2.)), np.log(g)))
+    return syn_em
+
+#Synchrotron self absorption coefficient (High Energy Radiation from Black Holes: Gamma Rays, Cosmic Rays, and Neutrinos by Charles D. Dermer and Govind Menon. Princeton Univerisity Press, 2009)
+def aSSA(N_el,B,nu,g,dg_l_el):
+    return q**3.*B*nu**(-5./3.)/(2.*m_el**2.*c**2.*0.8975)*np.trapz((2.*nu_c(g[1:-1],B))**(-1./3.)*np.exp(-nu/nu_c(g[1:-1],B))*(np.diff(N_el[1:])/dg_l_el-2.*N_el[1:-1]),np.log(g[1:-1]))
+
+#Synchrotron self absorption frequency (determined by solving tau_ssa = 1)
+def SSA_frequency(index_SSA,nu,aSSA_space,R):
+    if index_SSA>0:
+        line_1 = LineString(np.column_stack((np.log10(nu[index_SSA-2:index_SSA+1]),np.log10(np.multiply(aSSA_space,-R)[index_SSA-2:index_SSA+1]))))
+        line_2 = LineString(np.column_stack((np.log10(nu[index_SSA-2:index_SSA+1]),np.zeros([3]))))
+        int_pt = line_1.intersection(line_2)
+        return (10**int_pt.x) 
+
+# Inverse Compton scattering emissivity  (from Blumenthal & Gould 1970, Rev. Mod. Phys. 42, 237)  
+def Q_IC_space_optimized(Np, g_el, nu_ic_temp, photons, nu_targ, index):
+    temp_int_in_en = []
+    for g_ind in range(0,len(g_el)):
+        q_IC = nu_ic_temp/(4.*nu_targ*g_el[g_ind]*(g_el[g_ind]-h*nu_ic_temp/(m_el*c**2.)))
+        if q_IC[0] > 0.:
+            function_IC = 2.*q_IC*np.log(q_IC)+(1.+2.*q_IC)*(1.-q_IC)+(h*nu_ic_temp/(g_el[g_ind]*m_el*c**2.-h*nu_ic_temp))**2./(1.+h*nu_ic_temp/(g_el[g_ind]*m_el*c**2.-h*nu_ic_temp))*(1.-q_IC)/2.
+            function_IC[function_IC < 0.] = 0.
+            temp_int_in_en.append(np.trapz(np.multiply(photons[:index],function_IC[:index]),np.log(nu_targ[:index])))
+        else:
+            temp_int_in_en.append(0.)
+    IC_em = sigmaT*c*0.75*np.trapz(Np*g_el**(-1.)*temp_int_in_en,np.log(g_el))       
+    return IC_em
+
+#gamma gamma absorption coefficient  ( Coppi P. S., Blandford R. D., 1990, MNRAS, 245, 453. doi:10.1093/mnras/245.3.453) 
+def a_gg(nu_ic,nu_target,photons_target):   
+    t_gg_m=[]
+    x = h*nu_target/(m_el*c**2.)
+    for nu_ic_el in nu_ic:
+        x_IC = h*nu_ic_el/(m_el*c**2.)
+        x_space = np.logspace(np.log10(1.3/x_IC),np.log10(max(x)),50)
+        if x_space[0] < x_space[1]:                   
+            photons_gg = 10**np.interp(np.log10(x_space),np.log10(x),np.log10(photons_target/(h)*m_el*c**2.))
+            t_gg_m.append(np.trapz(0.652*sigmaT*((x_space*x_IC)**2.-1.)/(x_space*x_IC)**3.*np.log(x_space*x_IC)*x_space*photons_gg,np.log(x_space)))
+        else: t_gg_m.append(0.)
+    return(t_gg_m)
+            
+#Pair creation from gamma gamma absorption dN/dVdtdg (Eq. 57 in Mastichiadis A., Kirk J. G., 1995, A\&A, 295, 613)
+def Q_ee_f(nu_target,photons_target,nu_ic,photons_IC,g,R0):
+    Q_ee_temp=[]
+    for g_e in g: 
+        if (2.*g_e) > 1.:
+            x_prime = np.logspace(np.log10((2.*g_e)**(-1.)),np.log10(h*nu_target[-1]/(m_el*c**2.)))
+            n_ph_prime = 10**np.interp(np.log10(x_prime),np.log10(h*nu_target/(m_el*c**2.)),np.log10(photons_target*m_el*c**2./h))
+            n_d_u = 2.*g_e*x_prime
+            n_g = 10.**np.interp(np.log10(2.*g_e*m_el*c**2./h),np.log10(nu_ic),np.log10(photons_IC))
+            R_gg = 2.61*(n_d_u**2.-1)/n_d_u**3.*np.log(n_d_u)
+            Q_ee_temp.append(n_g*np.trapz(n_ph_prime*R_gg*x_prime,np.log(x_prime)))            
+        else: Q_ee_temp.append(0.)
+    return(np.multiply(c*sigmaT*m_el*c**2./h,Q_ee_temp[:-1]))
+
+#computes total photon spectrum by adding different spectral components
+def photons_tot(nu_syn,nu_bb,photons_syn,nu_ic,photons_IC,nu_tot,photons_bb,photons_pl,photons_user):
+    return 10**(np.interp(np.log10(nu_tot),np.log10(nu_bb),np.log10(photons_bb)))+10**(np.interp(np.log10(nu_tot),np.log10(nu_syn),np.log10(photons_syn)))+10**(np.interp(np.log10(nu_tot),np.log10(nu_ic),np.log10(photons_IC)))+photons_pl+photons_user
+
+#computes energy density of target photons for ICS scattering in Thomson limit   
+def U_ph_f(g,nu_target,photons_target,R):
+    U_ph_temp=[]
+    U_ph_tot=m_el*c**2./(sigmaT*R)*np.trapz(nu_target[:-1]*photons_target[:-1]*(sigmaT*R*m_el*c**2./h),nu_target[:-1])
+    for l_f in g:
+        if (3.*m_el*c**2.)/(4.*h*l_f)<nu_target[-1]:
+            photons_temp_v=np.interp(np.log10((3.*m_el*c**2.)/(4.*h*l_f)),np.log10(nu_target),np.log10(photons_target))
+            index_temp=max(max(np.where(np.log10(nu_target) < np.log10((3.*m_el*c**2.)/(4.*h*l_f))))+1)
+            photons_temp=np.insert(photons_target,index_temp, 10**photons_temp_v)
+            nu_temp=np.insert(nu_target,index_temp, (3.*m_el*c**2.)/(4.*h*l_f))
+            n_d_targ=h*nu_temp/(m_el*c**2.)
+            U_ph_temp.append(m_el*c**2./(sigmaT*R)*np.trapz(n_d_targ[:index_temp+1]*photons_temp[:index_temp+1]*(sigmaT*R*m_el*c**2./h),n_d_targ[:index_temp+1]))
+        else: U_ph_temp.append(U_ph_tot)
+    return(U_ph_temp)
+
+#computes correction factor in electron injection luminosity by checking the energy balance in a fast synchrotron cooling scenario
+def cor_factor_syn_el(g_el_space,R0,B0,p_el,Lum_e_injected):
+    #Constants
+    time_init = 0.
+    time_end = 20.
+    step_alg = 1.
+
+    g_el = g_el_space
+    g_el_mp = np.array([(g_el[im+1]+g_el[im-1])/2. for im in range(0,len(g_el)-1)])
+    C_syn_el = sigmaT*c/(h*24.*np.pi**2.*0.8975)*(4.*np.pi*m_el*c/(3.*q))**(4./3.)
+    const_el = 4./3.*sigmaT/(8.*np.pi*m_el*c)
+    dg_el = np.array([((g_el[im+1])-(g_el[im-1]))/2. for im in range(1,len(g_el)-1)])
+
+    #gamma-nu space
+    nu_syn = np.logspace(7.5,np.log10(7.*nu_c(g_el[-1],B0))+1.4,100)
+    day_counter=0.
+
+    #Initialize values for particles and photons
+    N_el = np.zeros(len(g_el))
+    el_inj = np.zeros(len(g_el))
+    photons_syn = np.ones(len(nu_syn))*10**(-260.)
+    N_el[0] = N_el[-1] = 10**(-160.)
+    
+    El_lum = []
+    Ph_lum = []
+    t = []
+    t_plot = []
+    time_real = time_init
+    el_inj = Q_el_Lum(Lum_e_injected,p_el,g_el[1],g_el[-2])*g_el**(-p_el)
+
+    while time_real <  time_end*R0/c:
+        dt = 0.1001*R0/c
+        time_real += dt    
+        t.append(time_real)
+        Radius = R0
+        B = B0
+        a_cr_el = 3.*q*B/(4.*np.pi*m_el*c)
+        
+        b_syn_el = const_el*B**2.
+        dgdt_Syn_el_m = b_syn_el*np.divide(np.power(g_el_mp[0:-1],2.),dg_el)
+        dgdt_Syn_el_p = b_syn_el*np.divide(np.power(g_el_mp[1:],2.),dg_el)
+
+        V1 = np.zeros(len(g_el)-2)
+        V2 = 1.+dt*(c/Radius+dgdt_Syn_el_m)
+        V3 = -dt*(dgdt_Syn_el_p) 
+        S_ij = N_el[1:-1]+el_inj[1:-1].copy()*dt
+        N_el[1:-1] = thomas(V1, V2, V3, S_ij)
+
+        # Q_Syn_el = [Q_syn_space(N_el/Volume(Radius),B,nu_syn[nu_ind],a_cr_el,C_syn_el,g_el) for nu_ind in range(len(nu_syn)-1)] 
+        Q_Syn_el = [Q_syn_space(N_el/Volume(Radius),B,nu_syn[nu_ind],a_cr_el,g_el) for nu_ind in range(len(nu_syn)-1)] 
+
+        V1 = np.zeros(len(nu_syn)-2)
+        V2 = 1.+dt*(c/R0*np.ones(len(nu_syn)-2))
+        V3 = np.zeros(len(nu_syn)-2)
+        S_ij = photons_syn[1:-1]+4.*np.pi*np.multiply(Q_Syn_el,dt)[1:]*Volume(Radius)
+        photons_syn[1:-1] = thomas(V1, V2, V3, S_ij )  
+        
+        if day_counter < time_real:
+            day_counter=day_counter+step_alg*R0/c
+            t_plot.append(time_real)
+            Syn_temp_plot = np.multiply(photons_syn/Volume(Radius),h*nu_syn**2.)*4.*np.pi/3.*Radius**2.*c
+            El_lum.append(np.trapz(el_inj*g_el**2.*m_el*c**2.,np.log(g_el)))
+            Ph_lum.append(np.trapz(Syn_temp_plot,np.log(nu_syn)))
+    return np.divide(Ph_lum,El_lum)[-1]
+
+    
+  
+def thomas(a,b,c,d):
+    """ A is the tridiagnonal coefficient matrix and d is the RHS matrix"""
+    N = len(a)
+    cp = np.zeros(N,dtype='float64') # store tranformed c or c'
+    dp = np.zeros(N,dtype='float64') # store transformed d or d'
+    X = np.zeros(N,dtype='float64') # store unknown coefficients
+    
+    # Perform Forward Sweep
+    # Equation 1 indexed as 0 in python
+    cp[0] = c[0]/b[0]  
+    dp[0] = d[0]/b[0]
+    # Equation 2, ..., N (indexed 1 - N-1 in Python)
+    for i in np.arange(1,(N),1):
+        dnum = b[i] - a[i]*cp[i-1]
+        cp[i] = c[i]/dnum
+        dp[i] = (d[i]-a[i]*dp[i-1])/dnum
+    
+    # Perform Back Substitution
+    X[(N-1)] = dp[N-1]  # Obtain last xn 
+
+    for i in np.arange((N-2),-1,-1):  # use x[i+1] to obtain x[i]
+        X[i] = (dp[i]) - (cp[i])*(X[i+1])
+    
+    return(X)
